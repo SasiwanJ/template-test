bec61303829dd843bc1832b513174b2fe2ae18b3
Sasiwan Janma <sasiwanj16@gmail.com>
2023-05-29 17:11:52 +0700

    add qoute

--- Quote-generator/script.js
+++ Quote-generator/script.js
@@ -1,1 +1,23 @@
-console.log('testing'); 
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a reandom quote apiQuote array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  
+}
+
+// Get Quote FRom API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error Here
+  }
+}
+
+// On Load
+getQuotes();
